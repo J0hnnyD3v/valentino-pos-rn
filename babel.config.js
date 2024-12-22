860e97d2138cd3f82234c579a0863624c239539b
Jonathan <jonathan@email.com>
2024-12-21 18:53:31 -0600

    chore: install expo router & nativewind

---
+++ babel.config.js
@@ -0,0 +1,26 @@
+const plugin = require("tailwindcss");
+
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: [
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "nativewind/babel",
+    ],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./src"],
+          extensions: [".ios.js", ".android.js", ".js", ".ts", ".tsx", ".json"],
+          alias: {
+            "@assets": "./assets",
+            "@components": "./src/components",
+            "@constants": "./src/constants",
+            "@hooks": "./src/hooks",
+          },
+        },
+      ],
+    ],
+  };
+};
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -5,8 +5,12 @@ import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
+import { Text, View } from 'react-native';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
+
+// Import your global CSS file
+import "../../global.css";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -14,7 +18,7 @@ SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require('@assets/fonts/SpaceMono-Regular.ttf'),
   });
 
   useEffect(() => {
@@ -29,10 +33,12 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
+      <View className='flex-1 pt-10 px-3 bg-light-background dark:bg-dark-background'>
+        <Text className='text-3xl text-light-text dark:text-dark-text'>Hola Mundo</Text>
+      </View>
+      {/* <Stack>
+        
+      </Stack> */}
       <StatusBar style="auto" />
     </ThemeProvider>
   );
